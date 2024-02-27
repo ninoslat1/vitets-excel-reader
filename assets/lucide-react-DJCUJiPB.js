215785bfe192121b5bfd5823691f9b3b71878ae5
ninoslat1 <benedict.ellnina@gmail.com>
2024-02-27 17:16:08 +0700

    update gh page

--- assets/lucide-react-DJCUJiPB.js
+++ assets/lucide-react-DJCUJiPB.js
@@ -1,4 +1,4 @@
-import{r as o}from"./react-Cd2k8YZK.js";/**
+import{r as o}from"./react-1F1He6-G.js";/**
  * @license lucide-react v0.334.0 - ISC
  *
  * This source code is licensed under the ISC license.
